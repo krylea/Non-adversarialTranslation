91d9142f6a5708cf4b652262e8293a819268faa8
Kira Selby <kiraselby@hotmail.com>
2019-05-16 13:40:58 -0400

    download embeddings

--- get_data.py
+++ get_data.py
@@ -10,7 +10,7 @@ import numpy as np
 import utils
 import argparse
 import os
-import wget
+#import wget
 
 vec_dir = "data"
 
@@ -25,6 +25,7 @@ def parse_args():
 
 params = parse_args()
 
+'''
 src_path = os.path.join(vec_dir, 'wiki.{}.vec'.format(params.src_lang))
 tgt_path = os.path.join(vec_dir, 'wiki.{}.vec'.format(params.tgt_lang))
 for l, p in [(params.src_lang, src_path), (params.tgt_lang, tgt_path)]:
@@ -35,6 +36,7 @@ for l, p in [(params.src_lang, src_path), (params.tgt_lang, tgt_path)]:
             _ = wget.download(vec_url, out=vec_dir)
         except:
             print('Download of {} vectors from {} failed.'.format(l, vec_url))
+'''
 
 src_id2word, src_word2id, src_embeddings = utils.read_txt_embeddings('data/wiki.%s.vec' % params.src_lang, params.n_init_ex, False)
 np.save('data/%s_%d' % (params.src_lang, params.n_init_ex), src_embeddings)
